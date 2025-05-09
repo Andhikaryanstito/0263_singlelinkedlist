b54c087a1897a9500ec8f009f51a4b1e66aaa083
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-05-09 08:04:23 +0700

    menambahkan fungsi addNode untuk menambah simpul ke list

--- file1.cpp
+++ file1.cpp
@@ -16,3 +16,10 @@ public:
     {
         START = NULL;
     }
+        void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
