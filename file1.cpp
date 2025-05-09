208210b9a35755f12caba25e2ddcb42e878e4aa9
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-05-09 08:09:06 +0700

    menambahkan fungsi Search untuk mencari node berdasarkan NIM

--- file1.cpp
+++ file1.cpp
@@ -50,4 +50,16 @@ public:
         nodeBaru->next = current;
         previous->next = nodeBaru;
     }
+
+  bool Search(int nim, Node *&previous, Node *&current)
+{
+    previous = START;
+    current = START;
+    while ((current != NULL) && (nim != current->noMhs))
+    {
+        previous = current;
+        current = current->next;
+    }
+    return (current != NULL);
 }
+};
